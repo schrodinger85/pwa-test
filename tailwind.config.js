cc38a25d8bffe905e4a290bbf0fd462528e86fd9
Nazmus Sakib <sakib6900@gmail.com>
2025-08-12 19:50:36 +0300

    ticket styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        gotham: ['gothamrnd', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // preview: {
   //   allowedHosts: ['3a3d0fdba0f7.ngrok-free.app']
   // },
-  plugins: [react(), VitePWA({
+  plugins: [react(),tailwindcss(), 
+    VitePWA({
     
     registerType: 'prompt',
     injectRegister: false,
