d4a3553b24b845ea3b04a1114262b7265e8900a2
Nazmus Sakib <sakib6900@gmail.com>
2025-08-12 19:05:45 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  preview: {
+    allowedHosts: ['3a3d0fdba0f7.ngrok-free.app']
+  },
+  plugins: [react(), VitePWA({
+    
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Avoin',
+      short_name: 'Avoin',
+      description: 'Avoin lippu Schrödingerin koiralta',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
